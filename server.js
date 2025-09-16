de4e398adca7100075b0cc14ba15c9dca38f061e
Sivaramakrishnan N <shivamaya192@gmail.com>
2025-09-16 08:04:28 +0530

    Updated

--- server.js
+++ server.js
@@ -57,6 +57,7 @@ app.post("/login", (req, res) => {
     .catch((err) => res.status(500).json({ error: err.message }));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
